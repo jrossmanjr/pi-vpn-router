19b5106ddb7bfaddc0001a8cc13cd0e28cf2d2bb
jrossmanjr <8453312+jrossmanjr@users.noreply.github.com>
2019-04-19 10:51:21 -0500

    Turned off Pi3 only HT_Capes

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 #--------------------------------------------------------------------------------------------------------------------#
-# MIT License 
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-#documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
-#the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+# MIT License
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+#documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+#the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 #and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
-#THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
-#OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+#THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+#OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 #OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #--------------------------------------------------------------------------------------------------------------------#
 
@@ -167,7 +167,7 @@ rsn_pairwise=CCMP
 macaddr_acl=0
 
 # these have to be set in agreement w/ channel and some other values... read hostapd.conf docs
-ht_capab=[HT20][SHORT-GI-20][DSSS_CCK-40]
+#ht_capab=[HT20][SHORT-GI-20][DSSS_CCK-40]
 
 # makes the SSID visible and broadcasted
 ignore_broadcast_ssid=0
